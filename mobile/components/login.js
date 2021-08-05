9af58665ab844a66d0de1e5be4e1ad4875647d91
Gordon Tse <gordon@pc-74-92.customer.ask4.lan>
2021-08-05 18:36:12 +0100

    updated login and create wallet frontend

--- mobile/components/login.js
+++ mobile/components/login.js
@@ -1,101 +1,104 @@
 import React from 'react';
-import {Text, Image, SafeAreaView, StyleSheet, Dimensions, Button, onPress, Alert, TextInput, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Dimensions,
+  Alert,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 const win = Dimensions.get('window');
 
-const Login = () => {
-    const text = React.useState(null);
-    const number = React.useState(null);
+const Wallet = () => {
+  const text = React.useState(null);
+  const number = React.useState(null);
 
-    return (
-        <SafeAreaView style={style.container}>
-            <Image 
-                source={require('../assets/scovpass_namelogo.png')}
-                style={style.frontLogo}
-            />
-            <SafeAreaView style={style.inputSection}>
-                <Text style={style.loginText}>Login to your wallet</Text>
-                <TextInput
-                    style={style.input}
-                    value={text}
-                    placeholder='your wallet name'
-                />
-                <TextInput
-                    style={style.input}
-                    value={number}
-                    placeholder='your passcode'
-                    keyboardType='number-pad'
-                    secureTextEntry={true}
-                />
-                <TouchableOpacity
-                        style={style.button}
-                        onPress={() => Alert.alert('Routing to be implemented')}>
-                        <Text style={style.buttonText}>Go to wallet</Text>
-                </TouchableOpacity>
-            </SafeAreaView>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={style.container}>
+      <Image
+        source={require('../assets/scovpass_namelogo.png')}
+        style={style.frontLogo}
+      />
+      <SafeAreaView style={style.inputSection}>
+        <Text style={style.loginText}>Login to your wallet</Text>
+        <TextInput style={style.input} placeholder="your wallet name" />
+        <TextInput
+          style={style.input}
+          placeholder="your passcode"
+          keyboardType="number-pad"
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          style={style.button}
+          onPress={() => Alert.alert('Routing to be implemented')}>
+          <Text style={style.buttonText}>Go to wallet</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
 };
 
 const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,        
-    },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
 
-    frontLogo: {
-        flex: 3,
-        width: win.width/1.2,
-        height: null,
-        resizeMode: 'contain',
-        alignSelf: 'center',
-    },
+  frontLogo: {
+    flex: 3,
+    width: win.width / 1.2,
+    height: null,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
 
-    inputSection: {
-        flex: 2,
-        alignContent: 'center',
-        justifyContent: 'center',
-        padding: 15,
-    },
+  inputSection: {
+    flex: 2,
+    alignContent: 'center',
+    justifyContent: 'center',
+    padding: 15,
+  },
 
-    loginText: {
-        fontSize: 22,
-        fontWeight: "300",
-        color: 'grey',
-        alignSelf: 'center',
-        padding: 10
-    },
+  loginText: {
+    fontSize: 22,
+    fontWeight: '300',
+    color: 'grey',
+    alignSelf: 'center',
+    padding: 10,
+  },
 
-    input: {
-        width: win.width/1.2,
-        height: 40,
-        margin: 5,
-        borderWidth: 0.5,
-        padding: 10,
-        fontSize: 18,
-        fontWeight: '200',
-        alignSelf: 'center',
-        borderColor: 'grey'
-    },
+  input: {
+    width: win.width / 1.2,
+    height: 40,
+    margin: 5,
+    borderWidth: 0.5,
+    padding: 10,
+    fontSize: 18,
+    fontWeight: '200',
+    alignSelf: 'center',
+    borderColor: 'grey',
+  },
 
-    button: {
-        width: win.width/1.2,
-        height: 42,
-        margin: 10,
-        padding: 10,
-        alignSelf: 'center',
-        backgroundColor:'#0f8df0',
-        borderRadius: 5
-    },
-    
-    buttonText: {
-        fontSize: 18,
-        fontWeight: "300",
-        color: 'white',
-        alignSelf: 'center',
-        justifyContent: 'center'
-    },
+  button: {
+    width: win.width / 1.2,
+    height: 42,
+    margin: 10,
+    padding: 10,
+    alignSelf: 'center',
+    backgroundColor: '#0f8df0',
+    borderRadius: 5,
+  },
+
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '300',
+    color: 'white',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
 });
 
-export default Login;
-
+export default Wallet;
