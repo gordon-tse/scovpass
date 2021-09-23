236245339b4eb0bef39a19c18e167599016bcd68
Gordon Tse <gordon@gordons-mbp.home>
2021-09-23 21:29:52 +0100

    clean up

--- enterpriseAPIs/app.js
+++ enterpriseAPIs/app.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
 
+const PORT = 7000;
+
+app.get("/", async (req, res) => {
+  res.send({ Working: "working now" });
+});
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
